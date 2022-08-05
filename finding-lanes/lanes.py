fd01f5153114b8321db3dab13815f2e28d38aeaa
AhmedSheded <Ahmed.shededmmm@gmail.com>
2022-08-06 01:06:56 +0200

    modified finding lanes

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -1,5 +1,10 @@
 import cv2
+import numpy as np
 
 image = cv2.imread('test_image.jpg')
-cv2.imshow("result", image)
+
+lane_image = np.copy(image)
+gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+
+cv2.imshow("result", gray)
 cv2.waitKey(0)
