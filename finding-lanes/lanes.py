3b095df7d773f33a1284701d647505ecc96de492
Ahmed Sheded <sheded222@gmail.com>
2022-08-31 18:21:39 +0200

    def display lines

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -8,11 +8,19 @@ lane_image = np.copy(image)
 
 
 def canny(image):
-
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
     canny = cv2.Canny(blur, 50, 100)
     return canny
+
+
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 5)
+    return line_image
 
 
 def region_of_interest(image):
@@ -27,5 +35,8 @@ def region_of_interest(image):
 
 canny = canny(lane_image)
 cropped_image = region_of_interest(canny)
-cv2.imshow("image", cropped_image)
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+line_image = display_lines(lane_image, lines)
+merged_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+cv2.imshow("image", merged_image)
 cv2.waitKey(5000)
